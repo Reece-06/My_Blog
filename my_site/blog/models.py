40f4f147d6f06cfbd243c99d8a7c262f14a7478f
Reece-06 <reece_depadua@dlsu.edu.ph>
2024-08-25 15:48:24 +0800

    form and classes done

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -7,7 +7,7 @@ from django.core.validators import MinLengthValidator
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     slug = models.SlugField(unique=True, db_index=True)
@@ -40,3 +40,9 @@ class Tag(models.Model):
 
     def __str__(self):
         return f"{self.caption}"
+    
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts', views.posts, name='posts-page'),
-    path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
+    path('', views.StartingPageView.as_view(), name='index'),
+    path('posts', views.AllPostsView.as_view(), name='posts-page'),
+    path('posts/<slug:slug>', views.PostDetailView.as_view(), name='post-detail-page'),
 ]
